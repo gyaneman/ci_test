b482ea104042c144d0cd9160dda104a05c294e3b
gyane <gyanexus7@gmail.com>
2015-07-09 02:07:24 +0900

    init

---
+++ ci_testprj/ci_testprj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ci_testprj
+//
+//  Created by 片岡崇史 on 2015/07/09.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
